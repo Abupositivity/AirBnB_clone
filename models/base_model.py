2d3cd8b9354aa2fffe763f66bc8899ffacf5749d
Abupositivity <abupositivity@gmail.com>
2024-03-11 03:51:28 +0000

    Bae  odel  pdate

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Contains the parent class BaseModel for AirBnB clone console.
 
 import uuid
 from datetime import datetime
-import models storage
+from models import storage
 
 
 class BaseModel:
@@ -22,19 +22,19 @@ class BaseModel:
         """
 
         if kwargs is not None and kwargs != {}:
-        for key in kwargs:
-            if key == "created_at":
-                self.__dict__["created_at"] = datetime.strptime(
-                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            elif key == "updated_at":
-                self.__dict__["updated_at"] = datetime.strptime(
-                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"\
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f"\
 )
-            else:
-                self.id = str(uuid.uuid4())
-                self.created_at = datetime.now()
-                self.updated_at = datetime.now()
-                storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Returns a human-readable string representation
@@ -44,7 +44,7 @@ class BaseModel:
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        "Updates the updated_at attribute
+        """Updates the updated_at attribute
         with the current datetime."""
 
         self.updated_at = datetime.now()
